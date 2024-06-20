4548c1743d66fd5226a99ce18b7141f01aeec74e
Yago Pajariño <yagopajarino@outlook.com>
2024-06-20 18:36:35 -0300

    Initial commit

---
+++ blog/2024-06-17-hello-world/index.md
@@ -0,0 +1,9 @@
+---
+title: Hello World
+authors: yago
+tags: [hello]
+image: https://i.imgur.com/mErPwqL.png
+hide_table_of_contents: false
+---
+
+Hola
---
+++ docusaurus.config.js
@@ -0,0 +1,84 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import { themes as prismThemes } from "prism-react-renderer";
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "Yago Pajariño",
+  favicon: "img/avatar.png",
+
+  // Set the production url of your site here
+  url: "https://blog.yagopajarino.ar",
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: "/",
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "yagopajarino", // Usually your GitHub org/user name.
+  projectName: "blog", // Usually your repo name.
+
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: false,
+        blog: {
+          routeBasePath: "/",
+          blogSidebarTitle: "All posts",
+          blogSidebarCount: "ALL",
+          showReadingTime: true,
+        },
+        theme: {
+          customCss: "./src/css/custom.css",
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      image: "img/avatar.jpg",
+      navbar: {
+        title: "YPB",
+        logo: {
+          alt: "My Site Logo",
+          src: "img/avatar.png",
+        },
+        items: [
+          {
+            href: "https://github.com/yagopajarino/blog",
+            label: "GitHub",
+            position: "right",
+          },
+        ],
+      },
+      footer: {
+        style: "dark",
+        copyright: `Copyright © ${new Date().getFullYear()} Yago Pajariño.`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
+};
+
+export default config;
