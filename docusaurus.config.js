7ea158c68d7613e6a6c3460f2d9ed1dafecc27f4
Yago Pajariño <yagopajarino@outlook.com>
2024-08-07 23:08:34 -0300

    Prepare for deploy

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -32,6 +32,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "yagopajarino", // Usually your GitHub org/user name.
   projectName: "blog", // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
