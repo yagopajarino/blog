af545c19e0b6b4959c997cc4b4f352e88026e679
Yago Pajariño <yagopajarino@outlook.com>
2024-08-20 20:01:14 -0300

    Deploy website - based on dee4ac5dbd0750bb1c32a0d1131dd57587ec41f9

---
+++ assets/js/1a64be3b.3be850a2.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkblog=self.webpackChunkblog||[]).push([[1459],{6004:a=>{a.exports=JSON.parse('{"tag":{"label":"gobierno","permalink":"/tags/gobierno","allTagsPath":"/tags","count":1,"unlisted":false},"listMetadata":{"permalink":"/tags/gobierno","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
