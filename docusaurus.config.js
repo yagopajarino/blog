3d1d02ba438320e55cef78231bcb1b73c17a01fb
Yago Pajariño <pajarinoyago@gmail.com>
2025-07-03 23:30:17 +0200

    fix: update baseUrl in docusaurus config for correct site deployment

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -26,7 +26,7 @@ const config = {
   url: "https://blog.yagopajarino.ar",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/blog/",
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
